82c23f7684cdf5a338e3a038284fc148dfc4e7ca
atsutakahiro <takahiro619z@icloud.com>
2023-05-23 21:41:16 +0900

    エラー

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,14 @@
 const { environment } = require('@rails/webpacker')
 
 module.exports = environment
+environment.config.merge({
+  resolve: {
+      fallback: {
+          dgram: false,
+          fs: false,
+          net: false,
+          tls: false,
+          child_process: false
+      }
+  }
+});
