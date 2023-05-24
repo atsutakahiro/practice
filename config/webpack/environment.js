fcd9bb872383f5a5c73742e415502bf518244a7a
atsutakahiro <takahiro619z@icloud.com>
2023-05-24 14:00:59 +0900

    ログイン、ログアウト機能確認

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,14 +1,3 @@
 const { environment } = require('@rails/webpacker')
 
 module.exports = environment
-environment.config.merge({
-  resolve: {
-      fallback: {
-          dgram: false,
-          fs: false,
-          net: false,
-          tls: false,
-          child_process: false
-      }
-  }
-});
